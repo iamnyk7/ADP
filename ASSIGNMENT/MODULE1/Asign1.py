88c2e095cdde4dfbe6560db32feaca837f275a47
Santhosh Nayak <48375730+iamnyk7@users.noreply.github.com>
2020-09-04 20:41:00 +0530

    Added Assignment1

---
+++ ASSIGNMENT/MODULE1/Asign1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter a integer:\n"))
+b=int(input("Enter another integer:\n"))
+print("Sum=",a+b,"\nDiffernece=",a-b,"\nProduct=",a*b,"\nInteger div=",a//b
+      ,"\nDivide=",a/b,"\nReminder=",a%b,"\nExponent=",a**b)
+print("----------------------------")
+a=float(input("Enter a floating number:\n"))
+b=float(input("Enter another floating number:\n"))
+print("Sum=",a+b,"\nDiffernece=",a-b,"\nProduct=",a*b,"\nInteger div=",a//b
+      ,"\nDivide=",a/b,"\nReminder=",a%b,"\nExponent=",a**b)
+
