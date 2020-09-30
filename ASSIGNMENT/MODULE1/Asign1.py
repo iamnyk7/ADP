27f699da6acc413c719046fa8c57c1b69721759d
codezy7 <64915468+codezy7@users.noreply.github.com>
2020-09-30 20:12:25 +0530

    Update Asign1.py

--- ASSIGNMENT/MODULE1/Asign1.py
+++ ASSIGNMENT/MODULE1/Asign1.py
@@ -1,10 +1,10 @@
-a=int(input("Enter a integer:\n"))
-b=int(input("Enter another integer:\n"))
-print("Sum=",a+b,"\nDiffernece=",a-b,"\nProduct=",a*b,"\nInteger div=",a//b
-      ,"\nDivide=",a/b,"\nReminder=",a%b,"\nExponent=",a**b)
+a=int(input("Enter first integer:\n"))
+b=int(input("Enter second integer:\n"))
+print("Sum=",a+b,"\nDifference=",a-b,"\nProduct=",a*b,"\nInteger Division=",a//b
+      ,"\nDivide=",a/b,"\nRemainder=",a%b,"\nExponent=",a**b)
 print("----------------------------")
-a=float(input("Enter a floating number:\n"))
-b=float(input("Enter another floating number:\n"))
-print("Sum=",a+b,"\nDiffernece=",a-b,"\nProduct=",a*b,"\nInteger div=",a//b
-      ,"\nDivide=",a/b,"\nReminder=",a%b,"\nExponent=",a**b)
+a=float(input("Enter first floating number:\n"))
+b=float(input("Enter second floating number:\n"))
+print("Sum=",a+b,"\nDiffernece=",a-b,"\nProduct=",a*b,"\nInteger Division=",a//b
+      ,"\nDivide=",a/b,"\nRemainder=",a%b,"\nExponent=",a**b)
 
